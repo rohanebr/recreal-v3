2cb39ff28d42bee74faa85347ac11e5111c8268e
Ahsin Anwar <ahsinanwar@gmail.com>
2014-07-10 15:46:45 +0500

    candidate edit profile

--- public/modules/candidate-features/controllers/candidate-cv.client.controller.js
+++ public/modules/candidate-features/controllers/candidate-cv.client.controller.js
@@ -15,6 +15,10 @@ angular.module('candidate-features').controller('CandidateCvController', ['$scop
 				$scope.candidate.skills =  [{title: ''}];
 			}
 
+			if(!candidate.educations){
+				$scope.candidate.educations =  [{degree: ''}];
+			}
+
 		});	
 
 		// Update existing Candidate
@@ -27,7 +31,7 @@ angular.module('candidate-features').controller('CandidateCvController', ['$scop
 				$scope.error = errorResponse.data.message;
 			});
 		};
-
+		//Skills
 	    $scope.addSkill = function() {
 	      $scope.candidate.skills.push({
 	        title: ''
@@ -37,6 +41,52 @@ angular.module('candidate-features').controller('CandidateCvController', ['$scop
 	    $scope.removeSkill = function(index) {
 	      $scope.candidate.skills.splice(index, 1);
 	    };
+
+	    //Education
+	     $scope.addEducation = function() {
+	      $scope.candidate.educations.push({
+	        title: ''
+	      });
+	    };
+
+	    $scope.removeEducation = function(index) {
+	      $scope.candidate.educations.splice(index, 1);
+	    };
+
+	    //Experience
+	     $scope.addExperience = function() {
+	      $scope.candidate.positions.push({
+	        title: ''
+	      });
+	    };
+
+	    $scope.removeExperience = function(index) {
+	      $scope.candidate.positions.splice(index, 1);
+	    };
+
+	    //Projects
+	     $scope.addProject = function() {
+	      $scope.candidate.projects.push({
+	        title: ''
+	      });
+	    };
+
+	    $scope.removeProject = function(index) {
+	      $scope.candidate.projects.splice(index, 1);
+	    };
+
+	    //Languages
+	     $scope.addLanguage = function() {
+	      $scope.candidate.languages.push({
+	        title: ''
+	      });
+	    };
+
+	    $scope.removeLanguage = function(index) {
+	      $scope.candidate.languages.splice(index, 1);
+	    };
+
+
 
 	}
 ]);
