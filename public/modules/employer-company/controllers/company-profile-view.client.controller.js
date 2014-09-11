d3bd3822063ee25fa388fd31c4bc9948b23714b3
Ahsin Anwar <ahsinanwar@gmail.com>
2014-09-11 18:00:02 +0500

    Update of Views

---
+++ public/modules/employer-company/controllers/company-profile-view.client.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+	angular.module('employer-company').controller('CompanyProfileViewController', ['$scope','Industries', 'Authentication', 'Employers', 'Companies', '$location',
+	function($scope, Industries, Authentication, Employers, Companies, $location) {
+		$scope.user = Authentication.user;
+		$scope.industries = Industries.getIndustries();
+		// If user is not signed in then redirect back home
+		if (!$scope.user) $location.path('/signin');
+
+		$scope.findOne = function(){
+			$scope.employer = Employers.get({
+				employerId: $scope.user.employer
+			}, function(employer){
+				$scope.company = Companies.get({
+					companyId: employer.company
+				});
+			});
+		};
+	}
+]);
