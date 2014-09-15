4210bc005c9591a6e66693c2d50a2f74b432f7ba
Mujahid <mujahid.iqbalsandhu@gmail.com>
2014-09-15 13:24:07 +0500

    branch check shortlist

--- public/modules/short-list/controllers/shortlisted-candidates.client.controller.js
+++ public/modules/short-list/controllers/shortlisted-candidates.client.controller.js
@@ -1,16 +1,18 @@
 'use strict';
 
-angular.module('short-list').controller('ShortlistedCandidatesController', ['$scope', '$http', '$stateParams',
-	function($scope, $http, $stateParams) {
+angular.module('short-list').controller('ShortlistedCandidatesController', ['$scope',
+	function($scope) {
 		// Controller Logic
 		// ...
 
-		$http.get('jobs/shortListedCandidates/' + $stateParams.jobId).success(function(job) {
+		$http.get('/jobs/candidates/:jobId' + $stateParams.jobId).success(function(job) {
 			$scope.job = job;
-			$scope.candidates = job.shortListedCandidates;
+			$scope.candidates = job.candidates;
 			$scope.filteredCandidates = $scope.candidates;
 
-
+			alert('page loaded');
+			alert($scope.candidates);
 		});
+		
 	}
 ]);
