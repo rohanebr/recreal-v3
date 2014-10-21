8819417991f7bd3061ca0907407dc68abe473fb0
Ahsin Anwar <ahsinanwar@gmail.com>
2014-10-21 15:49:53 +0500

    git shit

--- app/routes/candidates.server.routes.js
+++ app/routes/candidates.server.routes.js
@@ -1,38 +1,29 @@
 'use strict';
 
 module.exports = function(app) {
-	var users = require('../../app/controllers/users');
-	var candidates = require('../../app/controllers/candidates');
+    var users = require('../../app/controllers/users');
+    var candidates = require('../../app/controllers/candidates');
 
-	// Candidates Routes
-
-
+    // Candidates Routes
 
 
-	app.route('/uploadpicture').post(users.requiresLogin, candidates.uploadPicture);
-	app.route('/uploads/fullsize/:file').get(candidates.getImage);
-		app.route('/candidates')
-    		.get(candidates.list);
-    		// .post(users.requiresLogin, candidates.create);
-
-    	app.route('/candidates/:candidateId')
-    		.get(candidates.read)
-    		.put(users.requiresLogin, candidates.hasAuthorization, candidates.update)
-    		.delete(users.requiresLogin, candidates.hasAuthorization, candidates.delete);
 
 
-<<<<<<< HEAD
+    app.route('/uploadpicture').post(users.requiresLogin, candidates.uploadPicture);
+    app.route('/uploads/fullsize/:file').get(candidates.getImage);
+        app.route('/candidates')
+            .get(candidates.list);
+            // .post(users.requiresLogin, candidates.create);
 
-=======
-<<<<<<< HEAD
-    	app.route('/candidates/deleteSkill/:candidateId')
-    		.put(users.requiresLogin, candidates.hasAuthorization, candidates.deleteSkill);
-    	app.route('/candidates/addSkill/:candidateId')
-            .put(users.requiresLogin, candidates.hasAuthorization, candidates.addSkill);
-=======
->>>>>>> bc7c08f54384832ddf2b2ddd3743aa1399c98768
-    	
-    	app.route('/candidates/addSkill/:candidateId')
+        app.route('/candidates/:candidateId')
+            .get(candidates.read)
+            .put(users.requiresLogin, candidates.hasAuthorization, candidates.update)
+            .delete(users.requiresLogin, candidates.hasAuthorization, candidates.delete);
+
+
+
+        
+        app.route('/candidates/addSkill/:candidateId')
             .put(users.requiresLogin, candidates.hasAuthorization, candidates.addSkill);
         app.route('/candidates/addExperience/:candidateId')
             .put(users.requiresLogin, candidates.hasAuthorization, candidates.addExperience);
@@ -47,23 +38,11 @@ module.exports = function(app) {
 
         app.route('/candidates/deleteSkill/:candidateId')
             .put(users.requiresLogin, candidates.hasAuthorization, candidates.deleteSkill);
-<<<<<<< HEAD
 
-=======
->>>>>>> 33c093656bfa041e27607497e3558a3b176ee750
->>>>>>> bc7c08f54384832ddf2b2ddd3743aa1399c98768
         app.route('/candidates/deleteExperience/:candidateId')
             .put(users.requiresLogin,candidates.hasAuthorization,candidates.deleteExperience);
         app.route('/candidates/deleteProject/:candidateId')
             .put(users.requiresLogin,candidates.hasAuthorization,candidates.deleteProject);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        app.route('/candidates/updateExperience/:candidateId')
-            .put(users.requiresLogin,candidates.hasAuthorization,candidates.updateExperience);
-        app.route('/candidates/updateSkill/:candidateId')
-    	   .put(users.requiresLogin,candidates.hasAuthorization,candidates.updateSkill);
->>>>>>> bc7c08f54384832ddf2b2ddd3743aa1399c98768
 
         app.route('/candidates/deleteEducation/:candidateId')
             .put(users.requiresLogin,candidates.hasAuthorization,candidates.deleteEducation);
@@ -75,7 +54,7 @@ module.exports = function(app) {
         app.route('/candidates/updateExperience/:candidateId')
            .put(users.requiresLogin,candidates.hasAuthorization,candidates.updateExperience);
         app.route('/candidates/updateSkill/:candidateId')
-    	   .put(users.requiresLogin,candidates.hasAuthorization,candidates.updateSkill);
+           .put(users.requiresLogin,candidates.hasAuthorization,candidates.updateSkill);
         app.route('/candidates/updateProject/:candidateId')
            .put(users.requiresLogin,candidates.hasAuthorization,candidates.updateProject);
         app.route('/candidates/updateEducation/:candidateId')
@@ -84,12 +63,8 @@ module.exports = function(app) {
            .put(users.requiresLogin,candidates.hasAuthorization,candidates.updateLanguage);
         app.route('/candidates/updateCertificate/:candidateId')
            .put(users.requiresLogin,candidates.hasAuthorization,candidates.updateCertificate);
-<<<<<<< HEAD
 
-=======
->>>>>>> 33c093656bfa041e27607497e3558a3b176ee750
->>>>>>> bc7c08f54384832ddf2b2ddd3743aa1399c98768
-    		// Finish by binding the Candidate middleware
-            	app.param('candidateId', candidates.candidateByID);
+            // Finish by binding the Candidate middleware
+                app.param('candidateId', candidates.candidateByID);
 
 };
