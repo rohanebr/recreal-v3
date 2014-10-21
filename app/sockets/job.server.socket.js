e478936daeb2c7ee726f0a8c0fbd1d9528cb6a09
asadullahbeg <asadullahbeg@outlook.com>
2014-10-21 21:23:46 +0000

    update

--- app/sockets/job.server.socket.js
+++ app/sockets/job.server.socket.js
@@ -6,6 +6,16 @@ exports.publishCreate = function(){
 	
 };
 
+
+//This function is run for employer. When
+//a candiate applies on a job employer gets notified
 exports.applicationReceived = function(data){
     io.sockets.emit('applied_on_job', data);
+};
+
+
+//This function is run for the employee. When the job 
+//is pushed candidate receives it
+exports.jobPosted = function(data){
+    io.sockets.emit('job_posted', data);
 };
