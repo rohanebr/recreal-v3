2a3698fe816167c45bd2ae9ab9f1349554db2829
linkstatic <@asadullahbeg@outlook.com>
2014-11-09 04:15:59 +0500

    minor bugs fixed

--- public/modules/empoyer-jobs/directives/filter-list.client.directive.js
+++ public/modules/empoyer-jobs/directives/filter-list.client.directive.js
@@ -8,7 +8,7 @@ angular.module('empoyer-jobs').directive('filterList', ['$compile',
 			link: function postLink(scope, element, attrs) {
 				// Test directive directive logic
 				// ...
-				
+				console.log(attrs.filterFlag);
             
                var filterName = attrs.filterName;
 				var filterNameString = "'" + attrs.filterName + "'";
@@ -22,7 +22,7 @@ angular.module('empoyer-jobs').directive('filterList', ['$compile',
             		filterNameString = "'" + attrs.filterName + "'";
             		count  = "'count'";
             		 	scope.dummyfilters=[];
-            		  
+            		   console.log("FILTERS "+filterName);
             		for(var h=0,j=v.length;h<j;h++)
 				{
 					if(filterName==v[h].type)
@@ -30,18 +30,17 @@ angular.module('empoyer-jobs').directive('filterList', ['$compile',
 				}
 
 			
-				var html =	'<article class="thumbnail">' + 
-		                       '<label>'+ filterName +'</label>' +
-		                        '<ul>' + 
+			var html =	'<article class="thumbnail">' + 
+ 		                      '<label>'+ filterName +'</label>' +
+ 		                        '<ul>' + 
 		                            '<li data-ng-repeat="'+ filterName +'Filter in dummyfilters | orderBy:'+ count + ':true  | limitTo: filterLimit">' + 
 		                            '<input type="checkbox" data-ng-click="filterChanged('+ filterNameString +','+filterName+'Filter.name)" data-ng-model="'+ filterName +'Filter.value" id="{{'+ filterName +' + '+ filterName +'Filter.name}}" />' + 
-		                            '<label ng-if="'+ filterName +'Filter.name" for="{{'+ filterName +' + '+ filterName +'Filter.name}}" >{{'+ filterName +'Filter.name}} ({{'+ filterName +'Filter.count}})</label>' + 
-		                            '<label ng-if="!'+ filterName +'Filter.name" for="{{'+ filterName +' + '+ filterName +'Filter.name}}" >Not Mentioned ({{'+ filterName +'Filter.count}})</label>' + 
-		                            '</li>' + 
-		                        '</ul>' + 
-		                        '<a href="" data-ng-if="'+ filterName +'Filters.length > filterLimit" data-ng-click="openFilterModal('+ filterName +'Filters, '+ filterNameString +')">more choices...</a>' + 
-		                    '</article>';
-				
+ 		                            '<label ng-if="'+ filterName +'Filter.name" for="{{'+ filterName +' + '+ filterName +'Filter.name}}" >{{'+ filterName +'Filter.name}} ({{'+ filterName +'Filter.count}})</label>' + 
+ 		                            '<label ng-if="!'+ filterName +'Filter.name" for="{{'+ filterName +' + '+ filterName +'Filter.name}}" >Not Mentioned ({{'+ filterName +'Filter.count}})</label>' + 
+ 		                            '</li>' + 
+ 		                        '</ul>' + 
+ 		                        '<a href="" data-ng-if="dummyfilters.length > filterLimit" data-ng-click="openFilterModal('+ filterName +'Filters, '+ filterNameString +')">more choices...</a>' + 
+ 		                    '</article>';
                 var e =$compile(html)(scope);
 
             	element.replaceWith(e);
