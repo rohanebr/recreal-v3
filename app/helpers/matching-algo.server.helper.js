7d31b683464394c22ce9aa06df29d9814b1a40d5
linkstatic <@asadullahbeg@outlook.com>
2014-11-10 23:05:31 +0500

    sortingalgo

---
+++ app/helpers/matching-algo.server.helper.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose'),
+  
+    Candidate = mongoose.model('Candidate');
+
+
+exports.calculateMatchPercent=function(candidates,precedence,job)
+
+{
+
+	//very important, important ,ok 
+	//50, 30, 10 
+console.log(job['career_level']);
+if(precedence)
+	console.log("PRECEDENCE PROVIDED BY EMPLOYER");
+else
+	{
+   precedence=[{name:"career_level",nameinjob:"career_level",precedence:50},{name:"salary_expectation",nameinjob:"salary_range",precedence:30}];
+
+	}
+    
+
+    for(var h=0,j=candidates.length;h<j;h++)
+    {var xp=0;
+
+        var candidate=candidates[h];
+
+             for(var g=0;g<precedence.length;g++)
+             	{   
+             		console.log(job[precedence[g].nameinjob]+" "+candidate[precedence[g].name]);
+                   if(job[precedence[g].nameinjob]==candidate[precedence[g].name])
+                   	   xp=xp+precedence[g].precedence;
+
+             	}
+
+
+     candidate.xp=xp;
+     candidate.markModified('xp');
+	 candidate.save();
+     console.log("XP"+xp);
+
+
+
+
+
+
+    } 
+
+
+
+
+
+
+
+
+
+
+};
