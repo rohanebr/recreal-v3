83cf7ff3f79f41f37cccbe133c5baf13cbdd65d1
Ahsin Anwar <ahsinanwar@gmail.com>
2014-12-04 13:31:43 +0500

    employer sign-up wizard

--- public/modules/employer-signup-wizard/config/employer-signup-wizard.client.routes.js
+++ public/modules/employer-signup-wizard/config/employer-signup-wizard.client.routes.js
@@ -5,6 +5,10 @@ angular.module('employer-signup-wizard').config(['$stateProvider',
 	function($stateProvider) {
 		// Employer signup wizard state routing
 		$stateProvider.
+		state('emp-wizard-two', {
+			url: '/emp-wizard-two',
+			templateUrl: 'modules/employer-signup-wizard/views/emp-wizard-two.client.view.html'
+		}).
 		state('emp-wizard-one', {
 			url: '/emp-wizard-one',
 			templateUrl: 'modules/employer-signup-wizard/views/emp-wizard-one.client.view.html'
