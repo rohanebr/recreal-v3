6f941fa27e092441a99d922eabf46316edb94adc
Ahsin Anwar <ahsinanwar@gmail.com>
2014-12-05 13:42:13 +0500

    Emp-Signup-wizard-post-job first

---
+++ public/modules/empoyer-jobs/controllers/emp-job-post-one.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('empoyer-jobs').controller('EmpJobPostOneController', ['$scope',
+	function($scope) {
+		// Controller Logic
+		// ...
+	}
+]);
---
+++ public/modules/empoyer-jobs/controllers/emp-job-post-two.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('empoyer-jobs').controller('EmpJobPostTwoController', ['$scope',
+	function($scope) {
+		// Controller Logic
+		// ...
+	}
+]);
