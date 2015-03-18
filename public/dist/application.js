efb95e5915f8e653ceb3ba88cef516ddc644ee60
muddaserahmed <muddaserahmed@gmail.com>
2015-03-18 02:37:15 -0700

    build

--- public/dist/application.js
+++ public/dist/application.js
@@ -3148,31 +3148,6 @@ angular.module('core').controller('HomeController', [
     });
   }
 ]);'use strict';
-angular.module('siginin-wizard').controller('SigninController', [
-  '$modalInstance',
-  '$scope',
-  '$modal',
-  '$location',
-  '$http',
-  function ($modalInstance, $scope, $modal, $location, $http) {
-    // Controller Logic
-    // ...
-    $scope.credentials = {};
-    $scope.signin = function () {
-      $scope.credentials.userType = 'employer';
-      $http.post('/empsignupwizard/signupemployer', $scope.credentials).success(function (response) {
-        //If successful we assign the response to the global user model
-        if (response.status) {
-          $location.path('/signup-email-activation');
-          $modalInstance.dismiss();
-        }  //And redirect to the index page
-           // $location.path('/');
-      }).error(function (response) {
-        $scope.error = response.message;
-      });
-    };
-  }
-]);'use strict';
 angular.module('core').controller('TestController', [
   '$scope',
   function ($scope) {
